1d2b555fe8049ee7a0dada921a2afc5e85fba514
AdamDalloul <adalloul@eastsideprep.org>
2023-05-06 21:16:01 -0700

    testing

--- group_project/java/script.js
+++ group_project/java/script.js
@@ -1,4 +1,32 @@
+// Function to toggle dark mode
 function dark_mode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+  
+    // Save dark mode setting to localStorage
+    var isDarkModeEnabled = element.classList.contains("dark-mode");
+    localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
+  }
+  
+  // Check if dark mode is enabled in localStorage
+  var isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+  
+  // Set dark mode class on body element
+  if (isDarkModeEnabled) {
+    document.body.classList.add("dark-mode");
+  }
+  
+  // Listen for changes to the dark mode setting
+  window.addEventListener("storage", function (event) {
+    if (event.key === "darkModeEnabled") {
+      // Update dark mode setting on all pages
+      var isDarkModeEnabled = event.newValue === "true";
+      var element = document.body;
+      if (isDarkModeEnabled) {
+        element.classList.add("dark-mode");
+      } else {
+        element.classList.remove("dark-mode");
+      }
+    }
+  });
+  
